090ac821a876ee0f16d3613ee357ec87181cff41
Mogomotsi <amg.seiphemo@gmail.com>
2024-09-28 19:16:54 +0200

    Added a function to compute the position of a ball when it collides with the edges of the rim.

--- balls.py
+++ balls.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 from numpy.polynomial import Polynomial as Poly
 
@@ -41,7 +40,12 @@ def assessOutcome(poly):
             outcome = "Score"
         elif poly(-RR) < (HR + R):
             # Ball collides with the  last edge of the rim. We need to model this.
+            # We may need to backtrack to identify the point of collision.
             outcome = "Collision with the last edge of the rim."
+
+            x, y = findPositionOfBallCollidingWithRimEdge(poly, -RR, HR)
+
+            return outcome, x, y
         elif poly(-RR - DB) < (HR + HB):
             # Ball bounces of the backboard. We must model this.
             outcome = "Collision with backbboard."
@@ -52,11 +56,44 @@ def assessOutcome(poly):
         # Ball collides with the rim
         # We model the collision
         outcome = "Collision with the first edge of the rim. Need to model this."
+
+        x, y = findPositionOfBallCollidingWithRimEdge(poly, RR, HR)
+        
+        return outcome, x, y
     else:
         # Undershot
         outcome = "Undershot"
 
-    return outcome
+    return outcome, None, None
+
+
+def findPositionOfBallCollidingWithRimEdge(poly: Poly, x_rim, y_rim):
+    '''
+        Input:
+            poly: The polynomial that models the path followed by a ball
+            (x_rim, y_rim): The cartesian coordinate of an edge of the rim
+    '''
+
+    # Let the Polynomial package do the heavy lifting
+    #   Construct the equation of a circle using the Polynomial package
+    #       (x - x_c)**2 + (y - y_c)**2 = R*R
+    #       where x = x_rim , y = y_rim and R is the radius of the ball
+    #       x_c is unknown and y_c = poly 
+
+    p1 = Poly([x_rim, -1], domain=poly.domain, window=poly.window)
+    p1 = p1 * p1
+    
+    p2 = poly - y_rim
+    p2 = p2 * p2
+    
+    p = p1 + p2 - R*R
+    
+    roots = p.roots()
+    real_roots = np.extract(np.logical_not(np.iscomplex(roots)), roots)
+    correct_root = np.extract(np.real(real_roots) >= x_rim, real_roots.real)
+    
+    return correct_root[0], poly(correct_root[0])
+
 
 
 # Distance between sensors in meters
@@ -90,14 +127,28 @@ for i in range(1, 7):
     # (Least squares) Fit the data to a second order polynomial
     x, y = transformToCartesianCoordinates(dist1, dist2)
 
-    poly = Poly.fit(x, y, deg=2)
+    poly = Poly.fit(x, y, deg=2, domain=[-15, 15], window=[-15, 15])
 
-    outcome = assessOutcome(poly)
+    # If x_c and y_c exist then they represent the center of the ball that 
+    # collides with either the ring or backboarc
+    outcome, x_c, y_c = assessOutcome(poly)
     print("Assessment: ", outcome)
 
     xs = np.linspace(-1, 10, 100)
-    plt.plot(xs, poly(xs))
-    plt.plot(x, y)
-    plt.plot([-RR, RR], [3.05, 3.05])
+
+    figure, axes = plt.subplots()
+
+    axes.plot(xs, poly(xs))
+    axes.plot(x, y)
+    # Plot a line that represents the rim
+    axes.plot([-RR, RR], [3.05, 3.05])
+
+    if x_c is not None:
+        print("Drawing a circle.")
+        circle = plt.Circle((x_c, y_c), R, fill=False)
+
+        axes.set_aspect( 1 )
+        axes.add_artist( circle )
+
     plt.show()
 
